73c5c16f142fe6dd0d687581e260589fc05738a6
chibiboba <jennyjwan2@gmail.com>
2021-11-05 16:43:58 -0700

    js 120 studying

--- Lesson 5/ttt_oloo.js
+++ Lesson 5/ttt_oloo.js
@@ -1,43 +1,44 @@
 let readline = require("readline-sync"); // first line in ttt.js
 
 // Square class is on top of file to ensure that Board's constructor knows about Square class
-class Square { // class is used to represent squares
-  static UNUSED_SQUARE = " "; // state
-  static HUMAN_MARKER = "X";
-  static COMPUTER_MARKER = "O";
+let Square = {
+  UNUSED_SQUARE: " ",
+  HUMAN_MARKER: "X",
+  COMPUTER_MARKER: "O",
 
-  constructor(marker = Square.UNUSED_SQUARE) { // creates new, unused squares
-    this.marker = marker; // marker is set to the default " "
-  }
+  init(marker = Square.UNUSED_SQUARE) {
+    this.marker = marker;
+    return this;
+  },
 
-  toString() { // custom toString for when the object is logged as string, so it doesn't log [object object] 
+  toString() {
     return this.marker;
-  }
+  },
 
   setMarker(marker) {
     this.marker = marker;
-  }
+  },
 
   isUnused() {
     return this.marker === Square.UNUSED_SQUARE;
-  }
+  },
 
   getMarker() {
-    return this.marker; // can use toString() but interest is value of marker, not what it looks like
-    // on the board, so using getMaker() makes intent easier to discern
-  }
-}
-class Board {
-  constructor () {
+    return this.marker;
+  },
+};
+
+let Board = {
+  init() {
     this.squares = {};
     for (let counter = 1; counter <= 9; ++counter) {
-      this.squares[String(counter)] = new Square();
-      // each key is paired with an object that contains a marker variable
-      // and also toString() method
+      this.squares[String(counter)] = Object.create(Square).init();
     }
-  }
 
-  display() {
+    return this;
+  },
+
+  display () {
     console.log("");
     console.log("     |     |");
     console.log(`  ${this.squares["1"]}  |  ${this.squares["2"]}  |  ${this.squares["3"]}`);
@@ -51,7 +52,7 @@ class Board {
     console.log(`  ${this.squares["7"]}  |  ${this.squares["8"]}  |  ${this.squares["9"]}`);
     console.log("     |     |");
     console.log("");
-  }
+  },
 
   // allows you to always display the board at the same location on the screen
   displayWithClear() {
@@ -59,20 +60,20 @@ class Board {
     // console.log("");
     console.log("");
     this.display();
-  }
+  },
 
   markSquareAt(key, marker) { // key is location choice, marker is HUMAN_MARKER
     this.squares[key].setMarker(marker);
-  }
+  },
 
   unusedSquares() {
     let keys = Object.keys(this.squares);
     return keys.filter(key => this.squares[key].isUnused());
-  }
+  },
 
   isFull() {
     return this.unusedSquares().length === 0;
-  }
+  },
 
   countMarkersFor(player, keys) { // key is array of keys from which to count player's markers
     let markers = keys.filter(key => {
@@ -80,36 +81,35 @@ class Board {
     });
 
     return markers.length;
-  }
-}
+  },
+};
 
-class Player {
-  constructor(marker) {
+let PlayerPrototype = {
+  initialize(marker) {
     this.marker = marker;
-  }
+    return this;
+  },
 
   getMarker() {
     return this.marker;
-  }
-}
+  },
+};
 
-class Human extends Player {
-  constructor() {
-    // refers to constructor method for parent class
-    super(Square.HUMAN_MARKER); // retrieves the HUMAN_MARKER from Square class
-    // now Human has a marker property & getMarker method property
-  }
-}
+let Human = Object.create(PlayerPrototype);
 
-class Computer extends Player {
-  constructor() {
-    // refers to constructor method for parent class
-    super(Square.COMPUTER_MARKER); // retrieves the COMPUTER_MARKER from Computer class
-  }
-}
+Human.init = function() {
+  return this.initialize(Square.HUMAN_MARKER);
+};
 
-class TTTGame {
-  static POSSIBLE_WINNING_ROWS = [
+let Computer = Object.create(PlayerPrototype);
+
+Computer.init = function () {
+  return this.initialize(Square.COMPUTER_MARKER);
+};
+
+
+let TTTGame = {
+  POSSIBLE_WINNING_ROWS : [
     ["1", "2", "3"],            // top row of board
     ["4", "5", "6"],            // center row of board
     ["7", "8", "9"],            // bottom row of board
@@ -118,13 +118,14 @@ class TTTGame {
     ["3", "6", "9"],            // right column of board
     ["1", "5", "9"],            // diagonal: top-left to bottom-right
     ["3", "5", "7"],            // diagonal: bottom-left to top-right
-  ];
+  ],
 
-  constructor() {
-    this.board = new Board();
-    this.human = new Human();
-    this.computer = new Computer();
-  }
+  init() {
+    this.board = Object.create(Board).init();
+    this.human = Object.create(Human).init();
+    this.computer = Object.create(Computer).init();
+    return this;
+  },
 
   play() {
     this.displayWelcomeMessage();
@@ -143,16 +144,16 @@ class TTTGame {
     this.board.displayWithClear();
     this.displayResults();
     this.displayGoodbyeMessage();
-  }
+  },
 
   displayWelcomeMessage() {
     console.clear();
     console.log("Welcome to Tic Tac Toe!");
-  }
+  },
 
   displayGoodbyeMessage() {
     console.log("Thanks for playing Tic Tac Toe! Goodbye!");
-  }
+  },
 
   displayResults() {
     if (this.isWinner(this.human)) {
@@ -162,7 +163,7 @@ class TTTGame {
     } else {
       console.log("A tie game. How boring.");
     }
-  }
+  },
 
   humanMoves() { // was firstPlayerMoves
     let choice;
@@ -180,7 +181,7 @@ class TTTGame {
 
     // mark the selected square with the human's marker(x)
     this.board.markSquareAt(choice, this.human.getMarker());
-  }
+  },
 
   computerMoves() {  // was secondPlayerMoves
     console.log("computer moves");
@@ -193,23 +194,23 @@ class TTTGame {
     } while (!validChoices.includes(choice));
 
     this.board.markSquareAt(choice, this.computer.getMarker());
-  }
+  },
 
   gameOver() {
     return this.board.isFull() || this.someoneWon();
-  }
+  },
 
   someoneWon() { // detect if someone won
     return this.isWinner(this.human) || this.isWinner(this.computer);
-  }
+  },
 
   isWinner(player) { // detect who won
     return TTTGame.POSSIBLE_WINNING_ROWS.some(row => {
       return this.board.countMarkersFor(player, row) === 3;
     });
-  }
-}
+  },
+};
 
-let game = new TTTGame();
+let game = Object.create(TTTGame).init();
+// console.log(game);
 game.play();
-
